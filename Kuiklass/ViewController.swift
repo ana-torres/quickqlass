61264270c6a00ef3999e53b8aed75e0579c65f51
Ana Torres Piedra <Ana@macbook-pro-de-ana.local>
2019-01-10 13:29:44 +0100

    Initial Commit

---
+++ Kuiklass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kuiklass
+//
+//  Created by Ana Torres Piedra on 10/01/2019.
+//  Copyright © 2019 Ana Torres. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
